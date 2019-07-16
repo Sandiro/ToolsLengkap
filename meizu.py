7347fbc4190583a0f3661a2102ba643b25e83032
Sandiro <52814410+Sandiro@users.noreply.github.com>
2019-07-16 20:01:02 +0700

    Update meizu.py

--- meizu.py
+++ meizu.py
@@ -4,7 +4,7 @@ print ("\033[1;32mSilahkan Masukkan Username & Password Anda")
 
 print ("\033[1;32matau silahkan Hubungi MRR ")
 
-username = 'Rahmad'      
+username = 'Ikhsan'      
 
 password = 'Jomblo123'
 
